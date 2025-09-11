2e9b5830ae04444336f7e2aaa3b662b919fe9784
Mohd Elham <muhammedelham@gmail.com>
2025-09-12 00:27:32 +0530

    api gateway superfs v15

--- src/main/java/com/elu/apigatewaysuperfs/config/CorsWebConfig.java
+++ src/main/java/com/elu/apigatewaysuperfs/config/CorsWebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 
 @Configuration
 public class CorsWebConfig {
@@ -13,9 +15,14 @@ public class CorsWebConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedOrigin("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5000"));
+        corsConfiguration.addExposedHeader("Authorization");
+        corsConfiguration.addExposedHeader("Set-Cookie");
+
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
